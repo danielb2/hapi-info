612bfa84184e3d957fb3c02cd28d87b1026c1f9a
Nitin Sharma <nitinsh99@gmail.com>
2019-09-01 15:59:39 -0700

    Updated to support hapi v17 (#21)
    * migrate to hapi v17
    
    * updated package.json
    
    * updated travis
    
    * updated travis
    
    * updated readme
    
    * parmeterized pkg

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 'use strict';
 // Load modules
 
-const Code = require('code');
-const Glue = require('glue');
-const Lab = require('lab');
+const Code = require('@hapi/code');
+const Glue = require('@hapi/glue');
+const Lab = require('@hapi/lab');
 
 
 // Test shortcuts
@@ -17,134 +17,112 @@ const it = lab.test;
 const internals = {};
 
 
-internals.prepareServer = function (options, callback) {
+internals.prepareServer = async function (options, callback) {
 
     const manifest = {
-        registrations: [
-            { plugin: { register: '../', options } },
-            { plugin: { register:'./plugins/blah.js', options: null } },
-            { plugin: { register: './plugins/main.js', options: null } }
-        ]
+        register: {
+            plugins: [
+                { plugin: '../', options },
+                { plugin: './plugins/blah.js', options: null },
+                { plugin: './plugins/main.js', options: null }
+            ]
+        }
     };
 
-    Glue.compose(manifest, { relativeTo: __dirname }, (err, server) => {
-
-        expect(err).to.not.exist();
-        return callback(err, server);
-    });
+    try {
+        const server = await Glue.compose(manifest, { relativeTo: __dirname });
+        return server;
+    }
+    catch (error) {
+        return error;
+    }
 };
 
 
 describe('routes', () => {
 
-    it('prints plugin and server information', (done) => {
+    it('prints plugin and server information', async () => {
 
-        internals.prepareServer({}, (err, server) => {
+        const server  = await internals.prepareServer({});
+        const res = await server.inject('/hapi-info');
 
-            server.inject('/hapi-info', (res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.result.server.node).to.equal(process.version);
+        expect(res.result.server.hapi).to.equal(server.version);
+        expect(res.result.server.info).to.equal(server.info);
+        expect(res.result.server.uptime).to.be.about(1,2);
+        expect(res.result.server.memoryUsage).to.be.an.object();
+        expect(res.result.server.cpuUsage).to.be.an.object();
 
-                expect(res.statusCode).to.equal(200);
-                expect(res.result.server.node).to.equal(process.version);
-                expect(res.result.server.hapi).to.equal(server.version);
-                expect(res.result.server.info).to.equal(server.info);
-                expect(res.result.server.uptime).to.be.about(1,2);
-                expect(res.result.server.memoryUsage).to.be.an.object();
-                expect(res.result.server.cpuUsage).to.be.an.object();
-                done(err);
-            });
-        });
     });
 
-    it('prints plugin and server to a different path', (done) => {
+    it('prints plugin and server to a different path', async () => {
 
-        internals.prepareServer({ path: '/foo' }, (err, server) => {
-
-            server.inject('/foo', (res) => {
-
-                expect(res.statusCode).to.equal(200);
-                expect(res.result.server.node).to.equal(process.version);
-                expect(res.result.server.hapi).to.equal(server.version);
-                expect(res.result.server.info).to.equal(server.info);
-                expect(res.result.server.uptime).to.be.about(1,2);
-                expect(res.result.server.memoryUsage).to.be.an.object();
-                expect(res.result.server.cpuUsage).to.be.an.object();
-                done(err);
-            });
-        });
+        const server = await internals.prepareServer({ path: '/foo' });
+        const res = await server.inject('/foo');
+        expect(res.statusCode).to.equal(200);
+        expect(res.result.server.node).to.equal(process.version);
+        expect(res.result.server.hapi).to.equal(server.version);
+        expect(res.result.server.info).to.equal(server.info);
+        expect(res.result.server.uptime).to.be.about(1,2);
+        expect(res.result.server.memoryUsage).to.be.an.object();
+        expect(res.result.server.cpuUsage).to.be.an.object();
     });
 
-    it('prints plugin and option information', (done) => {
+    it('prints plugin and option information', async () => {
 
-        internals.prepareServer({ options: true }, (err, server) => {
+        const server = await internals.prepareServer({ options: true });
+        const res = await server.inject('/hapi-info');
 
-            server.inject('/hapi-info', (res) => {
-
-                expect(res.statusCode).to.equal(200);
-                const info = server.plugins['hapi-info'].info();
-                expect(info.plugins[0].options).to.equal({ options: true });
-                expect(res.result.plugins[0].options).to.equal({ options: true });
-                expect(res.result.plugins[1].options).to.equal(null);
-                expect(res.result.plugins[2].options).to.equal(null);
-                expect(res.result.server.node).to.equal(process.version);
-                expect(res.result.server.hapi).to.equal(server.version);
-                expect(res.result.server.info).to.equal(server.info);
-                expect(res.result.server.uptime).to.be.about(1,2);
-                expect(res.result.server.memoryUsage).to.be.an.object();
-                expect(res.result.server.cpuUsage).to.be.an.object();
-                done(err);
-            });
-        });
+        expect(res.statusCode).to.equal(200);
+        const info = server.plugins['hapi-info'].info();
+        expect(info.plugins[0].options).to.equal({ options: true });
+        expect(res.result.plugins[0].options).to.equal({ options: true });
+        expect(res.result.plugins[1].options).to.equal(null);
+        expect(res.result.plugins[2].options).to.equal(null);
+        expect(res.result.server.node).to.equal(process.version);
+        expect(res.result.server.hapi).to.equal(server.version);
+        expect(res.result.server.info).to.equal(server.info);
+        expect(res.result.server.uptime).to.be.about(1,2);
+        expect(res.result.server.memoryUsage).to.be.an.object();
+        expect(res.result.server.cpuUsage).to.be.an.object();
     });
 
-    it('prints plugin and server info using expose', (done) => {
+    it('prints plugin and server info using expose', async () => {
 
-        internals.prepareServer({ path: '/foo' }, (err, server) => {
-
-            const info = server.plugins['hapi-info'].info();
-            expect(info.plugins[0].options).to.not.exist();
-            expect(info.server.node).to.equal(process.version);
-            expect(info.server.hapi).to.equal(server.version);
-            expect(info.server.info).to.equal(server.info);
-            expect(info.server.uptime).to.be.about(1,2);
-            expect(info.server.memoryUsage).to.be.an.object();
-            expect(info.server.cpuUsage).to.be.an.object();
-            done(err);
-        });
+        const server = await internals.prepareServer({ path: '/foo' });
+        const info = server.plugins['hapi-info'].info();
+        expect(info.plugins[0].options).to.not.exist();
+        expect(info.server.node).to.equal(process.version);
+        expect(info.server.hapi).to.equal(server.version);
+        expect(info.server.info).to.equal(server.info);
+        expect(info.server.uptime).to.be.about(1,2);
+        expect(info.server.memoryUsage).to.be.an.object();
+        expect(info.server.cpuUsage).to.be.an.object();
     });
 
-    it('setting path to null doesnt create route, but still exposes info data', (done) => {
+    it('setting path to null doesnt create route, but still exposes info data', async () => {
 
-        internals.prepareServer({ path: null }, (err, server) => {
-
-            server.inject('/hapi-info', (res) => {
-
-                expect(res.statusCode).to.equal(404);
-                const info = server.plugins['hapi-info'].info();
-                expect(info.server.node).to.equal(process.version);
-                done(err);
-            });
-        });
+        const server = await internals.prepareServer({ path: null });
+        const res = await server.inject('/hapi-info');
+        expect(res.statusCode).to.equal(404);
+        const info = server.plugins['hapi-info'].info();
+        expect(info.server.node).to.equal(process.version);
     });
 
-    it('returned plugins can be filtered', (done) => {
+    it('returned plugins can be filtered', async () => {
 
-        internals.prepareServer({ path: null, pluginFilter: '^(?!hapi)' }, (err, server) => {
-
-            const info = server.plugins['hapi-info'].info();
-            expect(info.plugins.length).to.equal(1);
-            expect(info.plugins[0].name).to.equal('hapi-info');
-            done(err);
-        });
+        const server = await internals.prepareServer({ path: null, pluginFilter: '^(?!hapi)' });
+        const info = server.plugins['hapi-info'].info();
+        expect(info.plugins.length).to.equal(1);
+        expect(info.plugins[0].name).to.equal('hapi-info');
     });
 
-    it('setting no plugin filter returns all plugins', (done) => {
+    it('setting no plugin filter returns all plugins', async () => {
 
-        internals.prepareServer({ path: null }, (err, server) => {
-
-            const info = server.plugins['hapi-info'].info();
-            expect(info.plugins.length).to.equal(3);
-            expect(info.plugins[0].name).to.equal('hapi-info');
-            done(err);
-        });
+        const server = await internals.prepareServer({ path: null });
+        const info = server.plugins['hapi-info'].info();
+        expect(info.plugins.length).to.equal(3);
+        expect(info.plugins[0].name).to.equal('hapi-info');
     });
 });
